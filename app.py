820daf2e243bcee8ca655d7cb322021c2cc0e3c8
DineshAmbatwar <30665470+dinesh8181@users.noreply.github.com>
2022-03-14 14:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = joblib.load(r'Model/Knn_model.pkl')
 
 st.set_page_config(page_title="Iris Flower Prediction",page_icon="Flower", layout="wide")
 
-features=['sepal.length','sepal.width','petal.length','petal.width']
+features=['sepal_length','sepal_width','petal_length','petal_width']
 
 
 st.markdown("<h1 style='text-align: center;'>Iris Flower Prediction App </h1>", unsafe_allow_html=True)
@@ -19,20 +19,16 @@ def main():
         
         st.subheader("Enter the input for following features:")
         
-        sepal.length = st.slider("Select sepal_length: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
-        sepal.width = st.slider("Select sepal_width: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
-        petal.length = st.slider("Select petal_length: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
-        petal.width = st.slider("Select petal_width: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
-        
+        sepal_length = st.slider("Select sepal_length: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
+        sepal_width = st.slider("Select sepal_width: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
+        petal_length = st.slider("Select petal_length: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
+        petal_width = st.slider("Select petal_width: ", min_value = 0.0, max_value = 10.0, step = 0.1, format = "%f")
         
         submit = st.form_submit_button("Predict")
     
     if submit:
-        
-        data = np.array([sepal.length,sepal.width,petal.length,petal.width])
-        
+        data = np.array([sepal_length,sepal_width,petal_length,petal_width])
         pred = get_prediction(data = data, model = pipe)
-        
         st.write(f"The predicted Iris flower is:  {pred[0]}")
         
 if __name__ == '__main__':
