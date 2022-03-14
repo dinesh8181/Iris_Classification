1c0aa656c5a8623331f8549ef4a0e8a383ac753a
DineshAmbatwar <30665470+dinesh8181@users.noreply.github.com>
2022-03-14 16:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def main():
     
     if submit:
         data = np.array([sepal_length,sepal_width,petal_length,petal_width])
-        pred = get_prediction(data = data, pipe = pipe)
+        pred = get_prediction(data = data, pipe = model)
         st.write(f"The predicted Iris flower is:  {pred[0]}")
         
 if __name__ == '__main__':
