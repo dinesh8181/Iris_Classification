eb0aba6f6d75605cd2eeee998bb4ba2f0fee497b
DineshAmbatwar <30665470+dinesh8181@users.noreply.github.com>
2022-03-14 19:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import numpy as np
 import joblib
 from sklearn.neighbors import KNeighborsClassifier
 from prediction import get_prediction
+from sklearn.metrics import confusion_matrix,classification_report,precision_score,recall_score,f1_score
+from sklearn.metrics import _dist_metrics
 
 model = joblib.load(r'Model/Knn_model.pkl')
 
